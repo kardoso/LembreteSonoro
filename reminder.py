e09bac9efb2e1c99f4451e8f77eb848e30657627
Samuel Cardoso <scksamuel.sc@gmail.com>
2018-12-27 00:04:05 -0300

    Adicionar funcionalidades

--- reminder.py
+++ reminder.py
@@ -1,5 +1,9 @@
+import os # usado para pegar diretório em que o programa está
 import tkinter as tk # módulo tkinter para exibir janelas
-from tkinter import messagebox # exibir caixa de diálogo
+from tkinter import messagebox # para exibir caixa de diálogo
+from tkinter import filedialog # para procurar/selecionar arquivo
+import winsound # para reproduzir o som (no windows)
+from datetime import timedelta # para converter segundos para o formato HH:MM:SS
 
 # Largura da janela
 window_width = 400
@@ -32,12 +36,18 @@ class Application(tk.Frame):
         self.pack()
         self.create_widgets()
 
+    # Adicionar campos ao programa
     def create_widgets(self):
+        #Remover todos os campos presentes no programa
+        for slave in self.grid_slaves():
+            slave.grid_remove()
+            slave.grid_forget()
+            slave.destroy()
         #Criar o botão para importar arquivos
-        importButton = tk.Button(
+        self.importButton = tk.Button(
             self,
             text = "Importar arquivo de áudio",
-            #command = setsound,
+            command = self.setsound,
             anchor = tk.CENTER,
             font = "Times 12 bold",
             #width = 100,
@@ -47,55 +57,56 @@ class Application(tk.Frame):
             relief = tk.GROOVE,
             justify="center")
         #Inserir botão na janela
-        importButton.grid(row=1, column=1, columnspan=2, sticky=tk.E+tk.W)
+        self.importButton.grid(row=1, column=1, columnspan=2, sticky=tk.E+tk.W)
         
+        #O path do arquivo a ser carregador
+        self.soundpath = ""
         #Variável de texto informando a situação do arquivo
         #Se foi importado ou não, ou se o arquivo é válido
-        fileResponse = tk.StringVar(value = "Nenhum arquivo selecionado")
+        self.fileResponse = tk.StringVar(value = "Nenhum arquivo selecionado")
         #Criar label para mostrar a situação do arquivo
-        fResponse = tk.Label(
+        self.fResponse = tk.Label(
             self,
             text= "Nenhum arquivo selecionado",
-            textvariable = fileResponse,
+            textvariable = self.fileResponse,
             font=("Helvetica",9),
             foreground = "red")
         #Inserir label na janela
-        fResponse.grid(row=2, column=1, columnspan=2)
+        self.fResponse.grid(row=2, column=1, columnspan=2)
 
         #Label para informar ao usuário o que fazer
-        periodLabel = tk.Label(
+        self.periodLabel = tk.Label(
             self,
             text = "Informe a periodicidade:",
             font=("Helvetica 10 bold"),
             justify="center")
         #Inserir label na janela
-        periodLabel.grid(row=4, column=1)
+        self.periodLabel.grid(row=4, column=1)
 
         #Variável com o período(tempo do ciclo do programa)
-        period = tk.IntVar(self, value=30)
+        self.period = tk.IntVar(self, value=30)
         #Input para entrada do dado period
-        enterperiod = tk.Entry(self, textvariable=period, font=("Helvetica 10"), justify='center')
+        self.enterperiod = tk.Entry(self, textvariable=self.period, font=("Helvetica 10"), justify='center')
         #Inserir entry na janela
-        enterperiod.grid(row=4, column=2)
+        self.enterperiod.grid(row=4, column=2)
 
         #Opções de tempo
-        OPTIONS = ["Horas", "Minutos", "Segundos"]
+        self.OPTIONS = ["Horas", "Minutos", "Segundos"]
         #Varíavel com o tipo de tempo, padrão "Minutos"
-        timeType = tk.StringVar(self, value=OPTIONS[1])
+        self.timeType = tk.StringVar(self, self.OPTIONS[1])
         #Menu dropdown com as opções de tempo
-        timeChooser = tk.OptionMenu(self, timeType, *OPTIONS)
+        self.timeChooser = tk.OptionMenu(self,self.timeType, *self.OPTIONS)
         #Inserir dropdown na janela
-        timeChooser.grid(row=5, column=2)
+        self.timeChooser.grid(row=5, column=2, sticky="ew")
 
         #Botão para iniciar o programa
-        startButton = tk.Button(
+        self.startButton = tk.Button(
             self,
-            text = "Rodar o programa",
-            #command = run,
+            text = "Iniciar",
+            state = tk.DISABLED,
+            command = self.run,
             anchor = tk.CENTER,
             font = "Times 12 bold",
-            width = 20,
-            height = 1,
             background = "blue",
             foreground = "white",
             activebackground = "white",
@@ -103,10 +114,137 @@ class Application(tk.Frame):
             relief = tk.GROOVE,
             justify="center")
         #Inserir o botão na janela
-        startButton.grid(row=7, column=1, columnspan=2, sticky=tk.E+tk.W)
+        self.startButton.grid(row=7, column=1, columnspan=2, sticky=tk.E+tk.W)
         
-        #Configurar as linhas vazias do programa para terem tamanho 30
-        self.grid_rowconfigure([3, 6], minsize=30)
+        #Configurar altura das linhas do programa
+        self.grid_rowconfigure([1,2,3,4,5,6,7], minsize=26)
+
+    # Definir o som que será reproduzido
+    def setsound(self):
+        #Pedir para o usuário selecionar o arquivo WAV
+        fpath = filedialog.askopenfile(
+            #Iniciar o diretório em que o programa está
+            initialdir = os.path.dirname(os.path.abspath(__file__)),
+            title="Selecione o áudio",
+            filetypes=(("Arquivos .wav","*.wav"),
+                ("all files","*.*"))
+            )
+        #Se o arquivo de áudio foi selecionado
+        if fpath is not None:
+            #Se o nome terminar com '.wav'
+            #winsound reproduz apenas arquivos .wav
+            if fpath.name.endswith('.wav'):
+                self.soundpath = fpath.name
+                self.fileResponse.set("Aquivo selecionado: "+str(fpath.name))
+                self.fResponse.config(fg="green")
+                self.startButton.config(state=tk.ACTIVE)
+            else:
+                self.fileResponse.set("Selecione um arquivo válido")
+                self.fResponse.config(fg="orange")
+                self.startButton.config(state=tk.DISABLED)
+        #Se não foi selecionado nenhum arquivo
+        else:
+            self.fileResponse.set("Nenhum arquivo selecionado")
+            self.fResponse.config(fg="red")
+            self.startButton.config(state=tk.DISABLED)
+
+    # Reproduzir som
+    def playsound(self):
+        #SND_LOOP - Reproduz com loop o usuário tomar alguma ação que pare o loop
+        #SND_ASYNC - Reproduz de forma assíncrona, sem travar o programa
+        winsound.PlaySound(self.soundpath, winsound.SND_LOOP | winsound.SND_ASYNC)
+    
+    # Parar de reproduzir som
+    def stopsound(self):
+        winsound.PlaySound(None, winsound.SND_ALIAS)
+
+    # Iniciar ciclo do programa
+    def run(self):
+        #Parar de raproduzir som se estiver reproduzindo
+        self.stopsound()
+        #Remover todos os campos presentes programa
+        for slave in self.grid_slaves():
+            slave.grid_remove()
+            slave.grid_forget()
+            slave.destroy()
+
+        #Label com a mensagem ao concluir a contagem
+        messageLabel = tk.Label(
+            self,
+            text="Se lembre de ...",
+            font=("Helvetica 12 bold"),
+            fg="red")
+        
+        #Criar uma label apenas para mostrar o texto
+        visualTimer = tk.Label(
+            self,
+            text="00:00:00",
+            font=("Helvetica 24 bold"),
+            fg="black")
+        #Incluir label no grid
+        visualTimer.grid(row=3, column=1, columnspan=2, sticky=tk.E+tk.W)
+        
+        #Recebe a variável que o usuário digitou convertida em segundos
+        timeinseconds = 0
+        if(self.timeType.get() == self.OPTIONS[0]):
+            #Horas
+            timeinseconds = (self.period.get() * 60) * 60
+        elif(self.timeType.get() == self.OPTIONS[1]):
+            #Minutos
+            timeinseconds = self.period.get() * 60
+        elif(self.timeType.get() == self.OPTIONS[2]):
+            #Segundos
+            timeinseconds = self.period.get()
+        
+        #Atualizar a label 'visualTimer'
+        #e quando o tempo chegar a zero reproduz o som
+        #mostra opções e mensagem de lembrete
+        def update_timer(t):
+            if(t >= 0):
+                #Atualizar o texto da label visualTimer no formato de horas
+                visualTimer.configure(text=str(timedelta(seconds=t)))
+                #Decrementar dos segundos
+                t -= 1
+                #Esperar 1 segundo e fazer a recursão
+                self.master.after(1000, update_timer, t)
+            else:
+                #Fazer a janela ficar na frente de tudo
+                self.master.attributes('-topmost', 'true')
+                #Restaurar/mostrar a janela
+                self.master.deiconify()
+                #Reproduzir o som
+                self.playsound()
+                #Alterar a cor do visualTimer(cronometro) para vermelho
+                visualTimer.config(fg="red")
+                #Incluir mensagem no grid no grid
+                messageLabel.grid(row=2, column=1, columnspan=2, sticky=tk.E+tk.W)
+                #Criar um botão para resetar "cronometro"
+                self.resetButton = tk.Button(
+                    self,
+                    text = "Me avise novamente",
+                    command = self.run,
+                    anchor = tk.CENTER,
+                    font = "Times 12 bold",
+                    height = 1,
+                    relief = tk.GROOVE,
+                    justify="center")
+                #Inserir botão na janela
+                self.resetButton.grid(row=5, column=1, sticky=tk.E+tk.W)
+                #Criar um botão para sair do programa
+                self.exitButton = tk.Button(
+                    self,
+                    text = "Para com isso!",
+                    command = on_closing,
+                    anchor = tk.CENTER,
+                    font = "Times 12 bold",
+                    height = 1,
+                    relief = tk.GROOVE,
+                    justify="center")
+                #Inserir botão na janela
+                self.exitButton.grid(row=5, column=2, sticky=tk.E+tk.W)
+
+        #Iniciar "cronometro"
+        update_timer(timeinseconds)
 
 #Protocolo de saída
 # Quando for fechar o programa executar essa função
