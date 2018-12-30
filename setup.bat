82c59b4a9052d6fc12dfbbd599ae66a4cb3a3bb5
Samuel Cardoso <scksamuel.sc@gmail.com>
2018-12-30 02:57:04 -0300

    Atualizar setup.bat
    Gerar a pasta "Lembrete Sonoro" dentro do diretório
    Excluir pastas criadas durante a build

--- setup.bat
+++ setup.bat
@@ -1,13 +1,9 @@
 pyinstaller.exe --onefile --noconsol --name "Lembrete Sonoro" --icon=res/reminder.ico reminder.py
 
-copy res dist
-cd dist
-mkdir res
-move reminder.ico res
-mkdir "Lembrete Sonoro"
-move res "Lembrete Sonoro"
-move "Lembrete Sonoro.exe" "Lembrete Sonoro"
-cd ..
+mkdir "Lembrete Sonoro/res"
+move "dist\Lembrete Sonoro.exe" "Lembrete Sonoro"
+copy "res" "Lembrete Sonoro/res"
 rd /s /q  __pycache__
 rd /s /q build
+rd /s /q dist
 del "Lembrete Sonoro.spec"
